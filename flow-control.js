bbeba1431d6433bc38e7795978c7081f6f103f1b
Padmakanth Rajendram <codse172f-032@student.nibm.lk>
2020-02-19 20:38:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,7 +26,12 @@ else {return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
-
+if (age >= 13 && age <= 19) {
+return "You are a teenager";
+} 
+else {return "You are not a teenager";
+  
+}
 }
 
 function switchAge(age) {
